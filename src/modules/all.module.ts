5f2d6487952207ed90d705e00df93f1ebd4e0dbf
Akmyrat Charyyev <123238321+Akmyrat17@users.noreply.github.com>
2025-01-10 10:43:25 +0500

    Update all.module.ts

--- src/modules/all.module.ts
+++ src/modules/all.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-import { CheckModule } from './check/check.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, CheckModule],
+  imports: [UsersModule, AuthModule],
 })
 export class AllModule {}
